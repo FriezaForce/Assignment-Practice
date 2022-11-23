3c645fe84bda9f0dad75e5bdbea17a07834517bc
Frieza <118882614+FriezaForce@users.noreply.github.com>
2022-11-23 21:14:28 +0530

    Delete Week 6 directory

--- Week 6/ASCII.py
+++
@@ -1,9 +0,0 @@
-import string
-
-for upper in string.ascii_uppercase:
-    print(f'ASCII for uppercase {upper} is {ord(upper)}')
-
-print("------------------------------------------------------")
-
-for lower in string.ascii_lowercase:
-    print(f'ASCII for lowercase {lower} is {ord(lower)}')
