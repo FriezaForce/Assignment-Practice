a6e76a193a6a00cdbef12042b05e8b3f50e0ce93
Frieza <118882614+FriezaForce@users.noreply.github.com>
2022-11-23 21:14:17 +0530

    Delete Week 5 directory

--- Week 5/Natural_number.py
+++
@@ -1,8 +0,0 @@
-number = int(input("Please Enter any Number: "))
-i = 1
-
-print("The List of Natural Numbers from 1 to {0} are".format(number)) 
-
-while ( i <= number):
-    print (i, end = '  ')
-    i = i + 1
--- Week 5/Sum_of_digit.py
+++
@@ -1,9 +0,0 @@
-n=int(input("Enter number"))
-def Sum(n):    
-    sum = 0
-    while (n != 0): 
-        sum = sum + (n % 10)
-        n = n//10
-    return sum    
-
-print("Sum of digits",Sum(n))
